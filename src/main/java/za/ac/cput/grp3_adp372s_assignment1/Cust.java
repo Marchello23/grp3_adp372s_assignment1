0c000435610fe99daef8f8f7bbcdcfb706cb6df8
YaasinCole <yaasincole1999@gmail.com>
2021-03-31 16:06:03 +0200

    first commit

---
+++ src/main/java/za/ac/cput/grp3_adp372s_assignment1/Cust.java
@@ -0,0 +1,40 @@
+package za.ac.cput.grp3_adp372s_assignment1;
+
+public class Cust {
+
+    private String custName;
+    private static int orderID;
+    private static String order;
+
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public static int getOrderID() {
+        return orderID;
+    }
+
+    public static void setOrderID(int orderID) {
+        Cust.orderID = orderID;
+    }
+
+    public static String getOrder() {
+        return order;
+    }
+
+    public static void setOrder(String order) {
+        Cust.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return "Cust{" +
+                "custName='" + custName + '\'' +
+                '}';
+    }
+}
